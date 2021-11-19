f2b88b6e80baed440bb6895a521a41642ef40e06
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-11-19 15:02:38 +0100

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,6 +7,7 @@ import "../styles/sidebar.css";
 import { IconButton } from "@material-ui/core";
 import Chats from "./Chats";
 import { connect } from "react-redux";
+import { useEffect, useState} from 'react'
 
 
 const mapToState = (state) => ({
@@ -15,26 +16,26 @@ const mapToState = (state) => ({
 const mapDispatchToProps = (dispatch) => ({});
 
 
-const SideBar = ({user}) => {
+const SideBar = () => {
 
 
 
-  const [user, setUser] = useState(null);
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  // const [user, setUser] = useState(null);
+  
 
-  useEffect(() => {
-    const friendId = conversation.members.find((m) => m !== currentUser._id);
+  // useEffect(() => {
+  //   const friendId = conversation.members.find((m) => m !== currentUser._id);
 
-    const getUser = async () => {
-      try {
-        const res = await axios("/users?userId=" + friendId);
-        setUser(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getUser();
-  }, [currentUser, conversation]);
+  //   const getUser = async () => {
+  //     try {
+  //       const res = await axios("/users?userId=" + friendId);
+  //       setUser(res.data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   getUser();
+  // }, [currentUser, conversation]);
 
 //   console.log(user.user.username)
 
